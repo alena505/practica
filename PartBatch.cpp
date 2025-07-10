5daf5e4c793f828596662e406916fced6063dfe7
alena505 <115369290+alena505@users.noreply.github.com>
2025-07-10 10:55:34 +0300

    Update PartBatch.cpp

--- PartBatch.cpp
+++ PartBatch.cpp
@@ -1,32 +1,54 @@
-﻿#pragma once
-#include <vector>
+#include "PartBatch.h"
 #include <fstream>
-//Класс партий(базовый)
-class PartBatch {
-private:
-	int norm_parts_;
-	int bad_parts_;
-	double porog_;
-	double fine_;
-public:
-	PartBatch(int norm_parts, int bad_parts, double porog, double fine);
-	virtual double AnalyzeBraka();
-	virtual bool IsBrak();
-	virtual double CalculateFine();
-	int GetNormParts();
-	int GetBadParts();
-};
-//Класс потомок стандартных деталей
-class StandardPartBatch : public PartBatch {
-public:
-	StandardPartBatch(int norm_parts, int bad_parts);
-	bool IsBrak() override;
-	double CalculateFine() override;
-};
-//класс потомок критических деталей
-class CriticalPartBatch : public PartBatch {
-public:
-	CriticalPartBatch(int norm_parts, int bad_parts);
-	bool IsBrak() override;
-	double CalculateFine() override;
-};
+
+//Конструктор базового класса
+PartBatch::PartBatch(int norm_parts, int bad_parts, double porog, double fine)
+    : norm_parts_(norm_parts), bad_parts_(bad_parts), porog_(porog), fine_(fine) {}
+
+//Анализ процента бракованных деталей
+double PartBatch::AnalyzeBraka() {
+    if (norm_parts_ == 0) return 0.0;
+    return (static_cast<double>(bad_parts_) / norm_parts_) * 100.0;
+}
+//Анализ является ли деталь бракованной
+bool PartBatch::IsBrak() {
+    return AnalyzeBraka() > porog_;
+}
+//Рассчёт штрафа
+double PartBatch::CalculateFine() {
+    return bad_parts_ * fine_;
+}
+
+//Класс потомка стандартные детали
+StandardPartBatch::StandardPartBatch(int norm_parts, int bad_parts)
+    : PartBatch(norm_parts, bad_parts, 5.0, 100000.0) {}
+
+
+//Виртуальная переопределённая функция анализа является ли деталь бракованной
+bool StandardPartBatch::IsBrak() {
+    return PartBatch::IsBrak();
+}
+//Виртуальная переопределённая функция расчёта штрафа
+double StandardPartBatch::CalculateFine() {
+    return PartBatch::CalculateFine();
+}
+
+//Класс потомка критических деталей
+CriticalPartBatch::CriticalPartBatch(int norm_parts, int bad_parts)
+    : PartBatch(norm_parts, bad_parts, 2.0, 500000.0) {}
+//Виртуальная переопределённая функция анализа является ли деталь бракованной
+bool CriticalPartBatch::IsBrak() {
+    return PartBatch::IsBrak();
+}
+//Виртуальная переопределённая функция расчёта штрафа
+double CriticalPartBatch::CalculateFine() {
+    return PartBatch::CalculateFine();
+}
+//Делаем функцию для того, чтобы приватное поле norm_parts было доступно с помощбю этой функции
+int PartBatch::GetNormParts() {
+    return norm_parts_;
+}
+
+int PartBatch::GetBadParts() {
+    return bad_parts_;
+}
