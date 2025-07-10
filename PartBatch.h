cd170038bcb2da47fd7b0db0b11015194da6e854
alena505 <115369290+alena505@users.noreply.github.com>
2025-07-10 10:56:11 +0300

    Update PartBatch.h

--- PartBatch.h
+++ PartBatch.h
@@ -1,68 +1,32 @@
-﻿#include "ProductionDepartment.h"
+#pragma once
 #include <vector>
 #include <fstream>
-
-
-using namespace std;
-
-//Функция добавления мастера
-void ProductionDepartment::AddMaster(WorkshopMaster* master) {
-	name_master_.push_back(master);
-}
-
-//Анализ мастеров
-vector<WorkshopMaster*> ProductionDepartment::AnalyzeMasters() {
-	vector<WorkshopMaster*> bad_masters;
-	for (auto* master : name_master_) {
-		bool has_narush = false;
-		for (PartBatch* part : master->details_) {
-			double defect_procent = part->AnalyzeBraka();
-
-			if (part->IsBrak()) {
-				has_narush = true;
-				break;
-			}
-			
-	
-
-		}
-		if (has_narush) {
-			bad_masters.push_back(master);
-		}
-		
-	}
-
-
-	return bad_masters;
-
-}
-//Общий расчёт
-double ProductionDepartment::CalculateGeneralBrak() {
-	double summa_fines = 0.0;
-	for (WorkshopMaster* master : name_master_) {
-		for (PartBatch* part : master->details_) {
-			summa_fines += part->CalculateFine();
-		}
-	}
-	return summa_fines;
-}
-
-//Сводка по деталям
-void ProductionDepartment::Svodka(int& summ_details, int& summ_defect_details, double& summ_avg_deffect_procent) {
-	summ_details = 0;
-	summ_defect_details = 0;
-	for (WorkshopMaster* master : name_master_) {
-		for (PartBatch* part : master->details_) {
-			summ_details += part->GetNormParts() + part->GetBadParts();
-			summ_defect_details += part->GetBadParts();
-		}
-	}
-
-	if (summ_details > 0) {
-		summ_avg_deffect_procent = (static_cast<double>(summ_defect_details) / summ_details) * 100.0;
-	}
-	else {
-		summ_avg_deffect_procent = 0.0;
-	}
-
-}
+//Класс партий(базовый)
+class PartBatch {
+private:
+	int norm_parts_;
+	int bad_parts_;
+	double porog_;
+	double fine_;
+public:
+	PartBatch(int norm_parts, int bad_parts, double porog, double fine);
+	virtual double AnalyzeBraka();
+	virtual bool IsBrak();
+	virtual double CalculateFine();
+	int GetNormParts();
+	int GetBadParts();
+};
+//Класс потомок стандартных деталей
+class StandardPartBatch : public PartBatch {
+public:
+	StandardPartBatch(int norm_parts, int bad_parts);
+	bool IsBrak() override;
+	double CalculateFine() override;
+};
+//класс потомок критических деталей
+class CriticalPartBatch : public PartBatch {
+public:
+	CriticalPartBatch(int norm_parts, int bad_parts);
+	bool IsBrak() override;
+	double CalculateFine() override;
+};
